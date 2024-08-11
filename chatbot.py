a6b60c828d82b837acb5a8b14b4a7a2734281d1d
Isra Rafique <israrafique2211@gmail.com>
2024-08-11 18:34:09 +0500

    Your commit message

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,20 @@
 import google.generativeai as genai
-
+import streamlit as st
 #add apikey here
 API_KEY="AIzaSyDXuiZzPCmavxg2DH7NN5_rvKO5nM0xNaA"
-genai.configure(api_key=[API_KEY])
+genai.configure(api_key=API_KEY)
 #initialize model
 model = genai.GenerativeModel('gemini-1.5-flash')
 #create a function to get response
 def GetResponseFromModel(user_input):
     response = model.generate_content(user_input)
-    response.text
+    return response.text
+st.title("Geminie Chatbot")
 
-user_input=input("Enter a prompt =")
-output=GetResponseFromModel(user_input)
+user_input=st.text_input("Enter your prompt")
+if st.button("Get Response"):
+    if user_input:
+        output=GetResponseFromModel(user_input)
+        st.write(f"Chatbot Response: {output}")
+    else:
+        st.write("Enter Promopt")
